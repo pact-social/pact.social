327048c9b3e047e87283217846f005640710aecd
skylar745 <67788159+skylar745@users.noreply.github.com>
2023-06-28 16:40:55 +0200

    add favicon

--- components/metas.tsx
+++ components/metas.tsx
@@ -44,6 +44,11 @@ export const Metas: FC<MetaProps> = ({ title, description, imageSrc }) => {
         name="twitter:image"
         content={imageSrc} />
       {/* <meta property="twitter:domain" content="testnet.pact.social" /> */}
+
+      <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
+      <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
+      <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
+      <link rel="manifest" href="/site.webmanifest" />
     </Head>
   )
 }
