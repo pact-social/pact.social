f36d88515948921b5b50b0c077178a5b914c17eb
skylar745 <67788159+skylar745@users.noreply.github.com>
2023-09-19 12:19:53 +0200

    add submit button component

---
+++ components/form/submitButton.tsx
@@ -0,0 +1,26 @@
+import { useFormContext } from "react-hook-form";
+
+export default function SubmitButton ({
+  name,
+  className,
+  type,
+}: {
+  name: string;
+  className?: string;
+  type?: "button" | "submit" | "reset" | undefined;
+}) {
+  const methods = useFormContext()
+  const {
+    formState: { isValid, isLoading, isSubmitting, isSubmitted, isSubmitSuccessful, errors },
+  } = methods
+
+  console.log('formstate', isLoading, isSubmitting, errors)
+  return (
+    <button 
+      type={type || "submit"}
+      className={`btn ${className} ${(isLoading || isSubmitting || !isValid) && 'btn-disabled'}`}>
+      {(isLoading || isSubmitting) && <span className="loading loading-spinner"></span>}
+      {isSubmitSuccessful ? <>success</> : (!isValid) ? <>complete to {name}</>: <>{name}</> }
+    </button>
+  )
+}
