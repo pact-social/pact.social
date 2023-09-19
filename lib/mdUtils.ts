838c24388d2b49be6b78d009152e812b0d7e54a1
skylar745 <67788159+skylar745@users.noreply.github.com>
2023-09-19 12:01:38 +0200

    fix html to markdown

--- lib/mdUtils.ts
+++ lib/mdUtils.ts
@@ -3,7 +3,9 @@ import remarkHtml from "remark-html";
 
 import rehypeParse from "rehype-parse";
 import rehypeRemark from "rehype-remark";
+import rehypeSanitize from 'rehype-sanitize';
 import remarkStringify from "remark-stringify";
+import rehypeFormat from 'rehype-format';
 
 export function markdownToHtml(markdownText: string) {
   const file = remark().use(remarkHtml).processSync(markdownText);
@@ -11,11 +13,15 @@ export function markdownToHtml(markdownText: string) {
 }
 
 export function htmlToMarkdown(htmlText: string) {
+  // clean html tags with missing space before new character
+  const htmlCleaned = htmlText ? htmlText.replaceAll(/<(\w+)>([\S\s]+(?=\s))(\s+<\/\1>)(?=\S+)/g, '<$1>$2</$1> ') : htmlText
   const file = remark()
-    .use(rehypeParse, { emitParseErrors: true, duplicateAttribute: false })
+    .use(rehypeParse, { emitParseErrors: true, duplicateAttribute: false, fragment: true })
+    .use(rehypeFormat)
+    .use(rehypeSanitize)
     .use(rehypeRemark)
     .use(remarkStringify)
-    .processSync(htmlText);
+    .processSync(htmlCleaned);
 
   return String(file);
 }
